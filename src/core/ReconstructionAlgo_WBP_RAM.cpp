fabec92c7f46ec1b353a38b4dfcc5515f26b5541
Parrot0219 <zhaozhan0219@gmail.com>
2021-07-31 19:18:51 +0800

    优化三角函数，优化循环条件等

--- src/core/ReconstructionAlgo_WBP_RAM.cpp
+++ src/core/ReconstructionAlgo_WBP_RAM.cpp
@@ -650,11 +650,25 @@ void ReconstructionAlgo_WBP_RAM::doReconstruction(map<string, string> &inputPara
         float *stack_recon[Ny]; // (x,z,y)
         for (int j = 0; j < Ny; j++) {
             stack_recon[j] = new float[Nx * h];
-            for (int i = 0; i < stack_orig.getNx() * h; i++) {
+#pragma omp simd parallel for
+            for (int i = 0; i < Nx * h; i++) {
                 stack_recon[j][i] = 0.0;
             }
         }
+        /*
+         *  pre_atan
+         */
 
+        float *atan_pre = new float[(Nx/2)*Ny];
+#pragma omp parallel for
+        for (int i=0;i<Ny;i++){
+            int y = i;
+            float y_norm = (y >= int(ceil(float(Ny + 1) / 2))) ? (y - Ny) : (y);
+#pragma omp simd
+            for (int j=0;j<Nx/2;j++){
+                atan_pre[j+i*(Nx/2)] = atan(y_norm / j * Nx / Ny);
+            }
+        }
 
 
 
@@ -866,20 +880,29 @@ void ReconstructionAlgo_WBP_RAM::doReconstruction(map<string, string> &inputPara
                     float pix =  ctf_para[n].getPixelSize();
                     float Cs =  ctf_para[n].getCs();
                     float x_real_l = 1.0 / (Nx * pix);
-
+                    n_zz += ((zz_r-zz_l-1)/defocus_step+1);
+                    fftwf_plan *plan_ifft =  new fftwf_plan[n_zz];
+                    for (int n_z=0;n_z<n_zz;n_z++) {
+                        plan_ifft[n_z] = fftwf_plan_dft_c2r_2d(Ny, Nx, reinterpret_cast<fftwf_complex *>(stack_corrected[n_z]), (float *) stack_corrected[n_z],
+                                                          FFTW_ESTIMATE);
+                    }
+#pragma omp parallel for
+                    for (int n_z=0;n_z<n_zz;n_z++) {
+                        memcpy(stack_corrected[n_z],bufc,Nxc * Ny*sizeof(float));
+                    }
                     // defocus1,defocus2,astig,lamba,phase_shift,w_sin,w_cos,pix,Cs
 #pragma omp parallel for
                     for (int zz = zz_l;
                          zz < zz_r; zz += defocus_step)    // loop over every height (correct with different defocus)
                     {
                         int n_z = (zz + zz_r) / defocus_step;
-                        memcpy(stack_corrected[n_z],bufc,Nxc * Ny*sizeof(float));
-                        fftwf_plan plan_ifft;
-#pragma omp critical
-                        {
-                            plan_ifft = fftwf_plan_dft_c2r_2d(Ny, Nx, reinterpret_cast<fftwf_complex *>(stack_corrected[n_z]), (float *) stack_corrected[n_z],
-                                                              FFTW_ESTIMATE);
-                        }
+//                        memcpy(stack_corrected[n_z],bufc,Nxc * Ny*sizeof(float));
+//                        fftwf_plan plan_ifft;
+//#pragma omp critical
+//                        {
+//                            plan_ifft = fftwf_plan_dft_c2r_2d(Ny, Nx, reinterpret_cast<fftwf_complex *>(stack_corrected[n_z]), (float *) stack_corrected[n_z],
+//                                                              FFTW_ESTIMATE);
+//                        }
 //                        ctf_correction_perbufc(Nx, Ny, defocus1,defocus2,astig,lambda,phase_shift,w_sin,w_cos,pix,Cs,
 //                                               flip_contrast, float(zz) + float(defocus_step - 1) / 2,stack_corrected[n_z]);
                         float z_offset = float(zz) + float(defocus_step - 1) * 0.5;
@@ -888,7 +911,6 @@ void ReconstructionAlgo_WBP_RAM::doReconstruction(map<string, string> &inputPara
                             float y=j;
                             float y_norm = (y >= int(ceil(float(Ny + 1) / 2))) ? (y - Ny) : (y);
                             float y_real = float(y_norm) / float(Ny) * (1 / pix);
-                            float y_real_2 = y_real*y_real;
                             // 特殊处理 x_norm = 0
                             {
                                 float ctf_now;
@@ -902,7 +924,7 @@ void ReconstructionAlgo_WBP_RAM::doReconstruction(map<string, string> &inputPara
                                 } else {
                                     alpha = 0.0;
                                 }
-                                float freq2 = y_real_2;
+                                float freq2 = y_real * y_real;
                                 float df_now =
                                          ((defocus1 + defocus2 - 2 * z_offset * pix) +
                                          (defocus1 - defocus2) * cos(2 * (alpha - astig))) * 0.5;
@@ -916,21 +938,29 @@ void ReconstructionAlgo_WBP_RAM::doReconstruction(map<string, string> &inputPara
                                 if (flip_contrast) {
                                     ctf_now = -ctf_now;
                                 }
-                                ctf_now = 1-(int)(((*((unsigned int*)&ctf_now)) >> 31) << 1);
-                                stack_corrected[n_z][j * Nxc] *= ctf_now;
-                                stack_corrected[n_z][1 + j * Nxc] *= ctf_now;
+                                if (ctf_now >= 0){
+                                    stack_corrected[n_z][j * Nxc] *= 1;
+                                    stack_corrected[n_z][1 + j * Nxc] *= 1;
+                                } else {
+                                    stack_corrected[n_z][j * Nxc] *= -1;
+                                    stack_corrected[n_z][1 + j * Nxc] *= -1;
+                                }
+//                                ctf_now = 1-(int)(((*((unsigned int*)&ctf_now)) >> 31) << 1);
+//                                stack_corrected[n_z][j * Nxc] *= ctf_now;
+//                                stack_corrected[n_z][1 + j * Nxc] *= ctf_now;
                             }
 #pragma omp simd
-                            for (int i = 2; i < Nxc; i += 2) {
+                            for (int i = 1; i < Nxc/2; i ++) {
                                 //float ctf_now = ctf.computeCTF2D(i / 2, j, Nx, Ny, true, flip_contrast, z_offset);
                                 float ctf_now;
 //                              float x = i / 2;
 //                              float x_norm = (x >= int((float(Nx + 2) / 2))) ? (x - Nx) : (x);
-                                float x_norm = i * 0.5;
+//                                float x_norm = i;
 //                              float x_real = float(x_norm) / float(Nx) * (1 / pix);
-                                float x_real = float(x_norm) * x_real_l;
-                                float alpha = atan(y_real / x_real);
-                                float freq2 = x_real * x_real + y_real_2;
+                                float x_real = float(i) * x_real_l;
+//                                float alpha = atan(y_real / x_real);
+                                float alpha = atan_pre[i+j*(Nx/2)];
+                                float freq2 = x_real * x_real + y_real*y_real;
                                 float df_now =
                                         ((defocus1 + defocus2 - 2 * z_offset * pix) +
                                          (defocus1 - defocus2) * cos(2 * (alpha - astig))) * 0.5;
@@ -944,25 +974,31 @@ void ReconstructionAlgo_WBP_RAM::doReconstruction(map<string, string> &inputPara
                                 if (flip_contrast) {
                                     ctf_now = -ctf_now;
                                 }
-                                ctf_now = 1-(int)(((*((unsigned int*)&ctf_now)) >> 31) << 1);
-                                stack_corrected[n_z][i + j * Nxc] *= ctf_now;
-                                stack_corrected[n_z][(i + 1) + j * Nxc] *= ctf_now;
+                                if (ctf_now >= 0){
+                                    stack_corrected[n_z][ i*2 + j * Nxc] *= 1;
+                                    stack_corrected[n_z][(i*2 + 1) + j * Nxc] *= 1;
+                                } else {
+                                    stack_corrected[n_z][i*2 + j * Nxc] *= -1;
+                                    stack_corrected[n_z][(i*2 + 1) + j * Nxc] *= -1;
+                                }
+//                                ctf_now = 1-(int)(((*((unsigned int*)&ctf_now)) >> 31) << 1);
+//                                stack_corrected[n_z][i + j * Nxc] *= ctf_now;
+//                                stack_corrected[n_z][(i + 1) + j * Nxc] *= ctf_now;
                             }
                         }
 
-                        fftwf_execute(plan_ifft);
+                        fftwf_execute(plan_ifft[n_z]);
                         //  fft2buf(image, bufc, Nx, Ny);
 //#pragma omp simd
 //                        for (int i = 0; i < Nxc * Ny; i++)   // normalization
 //                        {
 //                            stack_corrected[n_z][i] = stack_corrected[n_z][i] / (Nx * Ny);
 //                        }
-#pragma omp critical
-                        {
-                            fftwf_destroy_plan(plan_ifft);
-                        }
+//#pragma omp critical
+//                        {
+//                            fftwf_destroy_plan(plan_ifft);
+//                        }
                     }
-                    n_zz += ((zz_r-zz_l-1)/defocus_step+1);
 #pragma omp parallel for
                     for (int n_z=0;n_z<n_zz;n_z++){
 #pragma omp simd
@@ -970,6 +1006,11 @@ void ReconstructionAlgo_WBP_RAM::doReconstruction(map<string, string> &inputPara
                         {
                             stack_corrected[n_z][i] = stack_corrected[n_z][i] / (Nx * Ny);
                         }
+                       // fftwf_destroy_plan(plan_ifft[n_z]);
+                    }
+
+                    for (int n_z=0;n_z<n_zz;n_z++){
+                        fftwf_destroy_plan(plan_ifft[n_z]);
                     }
                     delete [] bufc;
                     fftwf_destroy_plan(plan_fft);
